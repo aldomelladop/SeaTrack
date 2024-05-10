9fa0ff6e83d01b20f0b7cd4e2b1d2f79aae12ca2
aldomelladop <aldomellado.1310@gmail.com>
2024-05-10 18:08:16 -0400

    funcional-Kalman filter added

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # SeaTrack
-Este proyecto desarrolla un sistema de posicionamiento GPS económico usando la plataforma ESP32, integrando tecnologías de GPS y Wi-Fi para transmitir datos a Google Sheets. Proporciona métricas como latitud, longitud, velocidad y conteo de satélites, ideal para seguimiento vehicular a bajo costo con funcionalidades de actualización remota.
+
+## Descripción del Proyecto
+Este proyecto consiste en un rastreador GPS desarrollado utilizando un módulo ESP32, que se caracteriza por su capacidad para procesar y filtrar datos de ubicación GPS en tiempo real. La implementación de un filtro de Kalman permite mejorar significativamente la precisión de los datos de localización. Este sistema es capaz de sincronizar la hora mediante el protocolo NTP (Network Time Protocol) y enviar los datos de ubicación, junto con su correspondiente marca de tiempo, a un servidor web. Este proyecto está orientado a aplicaciones como seguimiento de vehículos, gestión de flotas y sistemas personales de navegación.
+
+## Características Principales
+- **Filtro Kalman**: Mejora la precisión de los datos GPS, reduciendo el ruido y las imprecisiones habituales en las lecturas de los módulos GPS.
+- **Sincronización de Hora NTP**: Proporciona una marca de tiempo precisa para cada registro de datos, crucial para el seguimiento temporal exacto en aplicaciones de monitoreo.
+- **Conectividad WiFi**: Permite la transmisión de datos a través de internet, facilitando la integración con sistemas basados en la nube y aplicaciones móviles.
+- **Actualizaciones OTA (Over-The-Air)**: Facilita la actualización del firmware del dispositivo sin necesidad de conexiones físicas, promoviendo un mantenimiento eficiente y a distancia.
+
+## Especificaciones de Hardware
+- **ESP32**: Utilizamos una placa genérica ESP32, la cual ofrece una excelente relación costo-beneficio, soporte comunitario amplio y facilidad de uso.
+- **Módulo GPS**: Recomendamos un módulo GPS como el NEO-6M, que ofrece buena precisión y es fácilmente integrable con ESP32.
+- **Fuente de Alimentación**: Dependiendo de la aplicación, se puede usar una batería recargable o una conexión directa a la red eléctrica del vehículo.
+
+## Configuración y Uso
+1. **Montaje del Hardware**: Conectar el módulo GPS al ESP32 siguiendo las instrucciones específicas de conexión UART.
+2. **Configuración de WiFi**: Ajustar los parámetros de conexión en el archivo `config.h` para asegurar la conectividad con la red local.
+3. **Programación y Despliegue**: Utilizar el IDE de Arduino para cargar el software al ESP32, asegurándose de seleccionar el modelo correcto de placa y puerto.
+
+## Proyectos Futuros
+- **Expansión a Monitoreo Ambiental**: Integración de sensores ambientales para monitorear condiciones como temperatura y humedad durante el transporte de mercancías sensibles.
+- **Sistema de Alertas en Tiempo Real**: Desarrollo de un sistema de notificaciones para alertar a los usuarios sobre eventos críticos, como desvíos de una ruta planificada o comportamientos de conducción peligrosos.
+
+## Otras Aplicaciones de Monitoreo
+- **Monitoreo de Mascotas**: Uso de collares GPS para la localización en tiempo real de mascotas.
+- **Sistemas de Seguridad Personal**: Implementación en dispositivos personales para alertar en situaciones de emergencia basándose en la localización del individuo.
+- **Logística y Distribución**: Optimización de rutas y seguimiento en tiempo real de la cadena de suministro.
+
+## Contribuciones
+Invitamos a desarrolladores y entusiastas a contribuir al proyecto, ya sea mejorando el código, proponiendo nuevas características o documentando casos de uso. Puedes contribuir haciendo fork del repositorio y enviando pull requests con tus mejoras.
+
+## Licencia
+Este proyecto se distribuye bajo la Licencia MIT, lo que permite la reutilización del código dentro de otros proyectos libremente y sin restricciones comerciales.
+
---
+++ lilygo_1005_1733/lilygo_1005_1733.ino
@@ -0,0 +1,239 @@
+#include "config.h"
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <TinyGPS++.h>
+#include <HardwareSerial.h>
+#include <Wire.h>
+#include <TimeLib.h>
+#include <ArduinoOTA.h>
+#include <ESP32Time.h>
+#include <SimpleKalmanFilter.h>
+
+SimpleKalmanFilter kalmanLat(1, 1, 0.01);
+SimpleKalmanFilter kalmanLng(1, 1, 0.01);
+
+ESP32Time rtc;
+const unsigned long GPS_UPDATE_INTERVAL = 300000;
+const unsigned long DATA_SEND_INTERVAL = 600000;
+unsigned long lastGPSUpdateTime = 0;
+unsigned long lastDataSendTime = 0;
+
+int numRedes = 3;
+TinyGPSPlus gps;
+HardwareSerial GPSSerial(1);
+String deviceID = "TBEAM-AM-0905-1040-SNRS";
+bool wifiConnected = false;
+const unsigned long samplingInterval = 60000;
+unsigned long lastSampleTime = 0;
+float h_nav = 0;
+unsigned long lastNavUpdateTime = 0;
+unsigned long lastGPSFixTime = 0;
+bool gpsFix = false;
+
+const int maxDataPoints = 5;
+int dataCount = 0;
+String dataBuffer[maxDataPoints];
+bool bufferEnabled = false;
+
+float filteredLat = 0.0;
+float filteredLng = 0.0;
+
+// Declaración de prototipos de funciones
+void configuraWiFi();
+void setupOTA();
+void muestraDebugGPS();
+void checkGPSFix();
+void updateKalmanFilters();
+String generaQueryString();
+void manageDataSending(const String& queryString);
+void enviaDatosGPS(const String& queryString);
+void verificaYReconectaWiFi();
+void verificaIntervalosYReinicia();
+void almacenarEnBuffer(const String& queryString);
+void enviarDatosDelBuffer();
+
+void setup() {
+    Serial.begin(115200);
+    GPSSerial.begin(9600, SERIAL_8N1, 34, 12);
+    configuraWiFi();
+    setupOTA();
+    configTzTime("CLT-3CLST,M10.3.0/0,M3.3.0/0", "pool.ntp.org");
+}
+
+void loop() {
+    ArduinoOTA.handle();
+    if (millis() - lastSampleTime >= samplingInterval) {
+        lastSampleTime = millis();
+        Serial.println("Tomando muestra del GPS...");
+        muestraDebugGPS();
+        checkGPSFix();
+        if (gps.location.isValid() && gpsFix) {
+            updateKalmanFilters();
+            String queryString = generaQueryString();
+            manageDataSending(queryString);
+        } else {
+            Serial.println("GPS: Buscando señal...");
+        }
+    }
+    verificaYReconectaWiFi();
+    verificaIntervalosYReinicia();
+}
+
+void muestraDebugGPS() {
+  while (GPSSerial.available() > 0) {
+    char c = GPSSerial.read();
+    if (gps.encode(c)) {
+      float measurementError = gps.hdop.value() * 0.5;  // Ajustable
+      kalmanLat.setProcessNoise(measurementError);
+      kalmanLng.setProcessNoise(measurementError);
+
+      filteredLat = kalmanLat.updateEstimate(gps.location.lat());
+      filteredLng = kalmanLng.updateEstimate(gps.location.lng());
+
+      Serial.print("GPS Filtrado: Lat = ");
+      Serial.print(filteredLat, 6);
+      Serial.print(", Lng = ");
+      Serial.println(filteredLng, 6);
+    }
+  }
+}
+
+void updateKalmanFilters() {
+    float measurementError = gps.hdop.value() * 0.1;
+    kalmanLat.setMeasurementError(measurementError);
+    kalmanLng.setMeasurementError(measurementError);
+
+    // Actualizar y almacenar las últimas estimaciones
+    filteredLat = kalmanLat.updateEstimate(gps.location.lat());
+    filteredLng = kalmanLng.updateEstimate(gps.location.lng());
+}
+
+String generaQueryString() {
+  time_t local = rtc.getEpoch();
+  tmElements_t tm;
+  breakTime(local, tm);
+  return String("?id=") + deviceID + "&lat=" + String(filteredLat, 6) + "&lng=" + String(filteredLng, 6) + "&hdop=" + String(gps.hdop.value());
+}
+
+void manageDataSending(const String& queryString) {
+  if (bufferEnabled) {
+    almacenarEnBuffer(queryString);
+  } else {
+    enviaDatosGPS(queryString);
+  }
+}
+
+void enviaDatosGPS(const String& queryString) {
+  HTTPClient http;
+  String fullURL = SERVER_URL + queryString;
+  http.begin(fullURL);
+  http.setTimeout(15000);
+  int httpCode = http.GET();
+
+  if (httpCode == HTTP_CODE_OK) {
+    Serial.println("Datos enviados exitosamente: " + http.getString());
+  } else {
+    Serial.print("Error en la solicitud HTTP: ");
+    Serial.println(http.errorToString(httpCode));
+  }
+  http.end();
+}
+
+// Esta función maneja la conexión inicial a la red WiFi.
+void configuraWiFi() {
+  for (int i = 0; i < numRedes; i++) {
+    Serial.print("Intentando conectar a: ");
+    Serial.println(ssids[i]);
+    WiFi.begin(ssids[i], passwords[i]);
+
+    int intentos = 0;
+    while (WiFi.status() != WL_CONNECTED && intentos < 10) {  // Intenta hasta 10 veces por red
+      delay(500);
+      Serial.print(".");
+      intentos++;
+    }
+
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("\nWiFi conectado.");
+      return;  // Sale de la función si se conecta exitosamente
+    } else {
+      Serial.println("\nFallo en la conexión. Intentando con la siguiente red...");
+    }
+  }
+
+  Serial.println("No se pudo conectar a ninguna red.");
+}
+
+void setupOTA() {
+  ArduinoOTA.setHostname(HOSTNAME);  // Set the hostname, defined in config.h
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start OTA");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd OTA");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+  });
+
+  ArduinoOTA.begin();
+  Serial.println("OTA Ready");
+}
+
+void verificaYReconectaWiFi() {
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("WiFi desconectado, intentando reconectar...");
+    configuraWiFi();
+    if (WiFi.status() == WL_CONNECTED) {
+      wifiConnected = true;
+      Serial.println("Reconectado a WiFi");
+    } else {
+      Serial.println("No se pudo reconectar a ninguna red.");
+    }
+  }
+}
+
+void verificaIntervalosYReinicia() {
+  unsigned long currentTime = millis();
+  if ((currentTime - lastGPSUpdateTime > GPS_UPDATE_INTERVAL) || (currentTime - lastDataSendTime > DATA_SEND_INTERVAL)) {
+    Serial.println("Reinicio necesario: intervalos máximos excedidos sin actualización del GPS o envío de datos.");
+    ESP.restart();  // Reinicia el microcontrolador
+  }
+}
+
+void checkGPSFix() {
+  if (gps.location.isValid()) {
+    lastGPSFixTime = millis();
+    gpsFix = true;
+    Serial.println("GPS fix adquirido.");
+  } else if (millis() - lastGPSFixTime > 300000) {  // 5 minutes without GPS fix
+    gpsFix = false;
+    Serial.println("GPS fix perdido por más de 5 minutos!");
+  }
+}
+
+void almacenarEnBuffer(const String& queryString) {
+  if (dataCount < maxDataPoints) {
+    dataBuffer[dataCount++] = queryString;
+  } else {
+    Serial.println("Buffer lleno, enviando datos al servidor...");
+    enviarDatosDelBuffer();
+  }
+}
+
+void enviarDatosDelBuffer() {
+  if (dataCount == 0) {
+    Serial.println("No hay datos para enviar");
+    return;
+  }
+
+  for (int i = 0; i < dataCount; i++) {
+    enviaDatosGPS(dataBuffer[i]);
+  }
+
+  dataCount = 0;  // Reinicia el contador del buffer
+}
