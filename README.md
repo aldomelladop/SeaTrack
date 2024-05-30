13531b4f0b6257f512e0580439532f17e6d279d2
aldomelladop <aldomellado.1310@gmail.com>
2024-05-30 15:16:16 -0400

    sleed mode

--- README.md
+++ README.md
@@ -20,20 +20,30 @@ Este proyecto desarrolla un sistema de rastreo y monitoreo para lanchas utilizan
 1. **Montaje del Hardware**: Conectar todos los sensores necesarios a la placa LilyGO T-Beam según las especificaciones de cada componente.
 2. **Configuración de la Red**: Ajustar los parámetros de WiFi y LoRa en el archivo `config.h` para garantizar una conectividad óptima.
 3. **Carga del Software**: Utilizar el IDE de Arduino para programar la placa, asegurándose de seleccionar la configuración adecuada para la T-Beam v1.1.
+4. **Configuración de Tiempos de Suspensión**: Ajustar los tiempos de suspensión para que el dispositivo entre en modo de suspensión después de transmitir datos exitosamente. Actualmente, el tiempo de espera es de 10 segundos y el intervalo de suspensión es de 1.5 minutos si está conectado a USB y 1.75 minutos si no está conectado a USB.
+
+### Configuración de `config.h`
+```cpp
+const char* ssids[] = {"WiFi_SSID1", "WiFi_SSID2"};
+const char* passwords[] = {"WiFi_PASSWORD1", "WiFi_PASSWORD2"};
+const char* SERVER_URL = "https://example.com/path_to_your_script";
+const char* HOSTNAME = "TBEAM-GPS-Device";
+const char* OTA_PASSWORD = "your_ota_password";
+char auth[] = "your_blynk_auth_token";
+```
 
 ## Proyectos Futuros y Ampliación
-- **Monitoreo Integral de Embarcaciones**: Incluir sensores para el seguimiento de la salud estructural del casco, sistemas eléctricos y eficiencia del motor.
-- **Bitácora Automática**: Desarrollar un sistema que genere registros automáticos de cada viaje, incluyendo rutas, condiciones operativas y eventos notables.
-- **Sistema de Alertas Proactivo**: Implementar un sistema de alertas que notifique a los operadores y a las bases en tierra sobre condiciones críticas o mantenimiento necesario.
+* **Monitoreo Integral de Embarcaciones**: Incluir sensores para el seguimiento de la salud estructural del casco, sistemas eléctricos y eficiencia del motor.
+* **Bitácora Automática**: Desarrollar un sistema que genere registros automáticos de cada viaje, incluyendo rutas, condiciones operativas y eventos notables.
+* **Sistema de Alertas Proactivo**: Implementar un sistema de alertas que notifique a los operadores y a las bases en tierra sobre condiciones críticas o mantenimiento necesario.
 
 ## Otras Aplicaciones de Monitoreo
-- **Seguridad Personal en Navegación**: Dispositivos personales que alerten sobre hombre al agua o desviaciones de una ruta planificada.
-- **Investigación Oceanográfica**: Uso de flotas de drones marinos equipados con similares sistemas de monitoreo para recopilación de datos marinos a gran escala.
-- **Turismo y Recreación**: Ofrecer a los operadores turísticos herramientas para el seguimiento y la gestión de sus flotas en tiempo real, mejorando la seguridad y la experiencia del usuario.
+* **Seguridad Personal en Navegación**: Dispositivos personales que alerten sobre hombre al agua o desviaciones de una ruta planificada.
+* **Investigación Oceanográfica**: Uso de flotas de drones marinos equipados con similares sistemas de monitoreo para recopilación de datos marinos a gran escala.
+* **Turismo y Recreación**: Ofrecer a los operadores turísticos herramientas para el seguimiento y la gestión de sus flotas en tiempo real, mejorando la seguridad y la experiencia del usuario.
 
 ## Contribuciones
 Este proyecto está abierto a colaboraciones. Se invita a los desarrolladores e investigadores interesados en tecnologías marinas a contribuir con código, ideas y pruebas de campo.
 
 ## Licencia
 Distribuido bajo la Licencia MIT, permitiendo el uso libre y la integración en aplicaciones comerciales sin restricciones.
-
--- TBEAM_29_05_sleep_mode.ino
+++ TBEAM_29_05_sleep_mode.ino
@@ -19,7 +19,7 @@ unsigned long lastDataSendTime = 0;
 
 TinyGPSPlus gps;
 HardwareSerial GPSSerial(1);
-String deviceID = "FCOB-TBEAM-2705";
+String deviceID = "SNST-TBEAM-3005";
 bool wifiConnected = false;
 const unsigned long samplingInterval = 60000;
 unsigned long lastSampleTime = 0;
@@ -28,76 +28,131 @@ unsigned long lastNavUpdateTime = 0;
 unsigned long lastGPSFixTime = 0;
 bool gpsFix = false;
 
-float filteredLat = 0.0;
-float filteredLng = 0.0;
+// Variables para el monitoreo de batería y USB
+const int batteryPin = 35; // Pin analógico para leer el voltaje de la batería
+const int usbPowerPin = 36; // Pin digital para detectar si está conectado a USB
+float batteryVoltage = 0.0;
+bool usbConnected = false;
+unsigned long sleepInterval = 90000; // 1.5 minutes by default
+
+// Variables para detección de interferencia
+const int RSSI_THRESHOLD = -80; // Umbral de RSSI en dBm
+int lastRSSI = 0;
+bool interferenceDetected = false;
 
 void configuraWiFi();
 void setupOTA();
 void muestraDebugGPS();
 void checkGPSFix();
 String generaQueryString();
-void enviaDatosGPS(const String& queryString);
+bool enviaDatosGPS(const String& queryString);
 void verificaYReconectaWiFi();
-void verificaIntervalosYReinicia();
 void sincronizaTiempo();
 void enviarDatosATiempo();
 void obtenerTiempo(struct tm &timeinfo);
+void verificarYEntrarSleep();
+void checkInterference();
 
 void setup() {
     Serial.begin(115200);
     GPSSerial.begin(9600, SERIAL_8N1, 34, 12);
+    pinMode(batteryPin, INPUT);
+    pinMode(usbPowerPin, INPUT);
     configuraWiFi();  // Ensure WiFi is connected before continuing
     setupOTA();
     sincronizaTiempo();  // Synchronize time with NTP server
     Blynk.config(auth);
     Blynk.connect();  // Iniciar Blynk
+
+    // Verificar si está conectado a USB
+    usbConnected = digitalRead(usbPowerPin);
+    if (!usbConnected) {
+        sleepInterval = 105000; // 1.75 minutes if not connected to USB
+    }
 }
 
 void loop() {
     ArduinoOTA.handle();
     Blynk.run();  // Ejecutar Blynk
-    unsigned long currentMillis = millis();
-    if (currentMillis - lastSampleTime >= samplingInterval) {
-        lastSampleTime = currentMillis;
+
+    // Obtener y transmitir datos GPS
+    obtenerYTransmitirDatosGPS();
+
+    // Esperar 10 segundos antes de entrar en modo de suspensión
+    delay(10000);
+
+    // Entrar en modo de suspensión
+    verificarYEntrarSleep();
+}
+
+void obtenerYTransmitirDatosGPS() {
+    bool datosTransmitidos = false;
+
+    while (!datosTransmitidos) {
         Serial.println("Tomando muestra del GPS...");
         muestraDebugGPS();
         checkGPSFix();
         if (gps.location.isValid() && gpsFix) {
+            lastNavUpdateTime = millis();
             String queryString = generaQueryString();
-            enviaDatosGPS(queryString);
-            enviarDatosATiempo();  // Enviar datos de tiempo a Blynk
+            datosTransmitidos = enviaDatosGPS(queryString);
+            if (datosTransmitidos) {
+                enviarDatosATiempo();  // Enviar datos de tiempo a Blynk
 
-            // Enviar coordenadas como string a Blynk
-            String coordenadas = String(filteredLat, 6) + ", " + String(filteredLng, 6);
-            Blynk.virtualWrite(V14, coordenadas);
+                // Enviar coordenadas como string a Blynk
+                String coordenadas = String(gps.location.lat(), 6) + ", " + String(gps.location.lng(), 6);
+                Blynk.virtualWrite(V14, coordenadas);
 
-            // Enviar fecha y hora como string a Blynk
-            struct tm timeinfo;
-            obtenerTiempo(timeinfo);
-            char fechaHora[20];
-            strftime(fechaHora, sizeof(fechaHora), "%d-%m-%y - %H:%M", &timeinfo);
-            Blynk.virtualWrite(V15, fechaHora);
+                // Enviar fecha y hora como string a Blynk
+                struct tm timeinfo;
+                obtenerTiempo(timeinfo);
+                char fechaHora[20];
+                strftime(fechaHora, sizeof(fechaHora), "%d-%m-%y - %H:%M", &timeinfo);
+                Blynk.virtualWrite(V15, fechaHora);
+            }
         } else {
             Serial.println("GPS: Buscando señal...");
         }
+
+        usbConnected = digitalRead(usbPowerPin);
+
+        // Enviar datos de batería y estado de conexión a Blynk
+        Blynk.virtualWrite(V13, usbConnected);
+
+        // Ajustar intervalo de suspensión si se detecta desconexión de USB
+        if (!usbConnected) {
+            sleepInterval = 105000; // 1.75 minutes if not connected to USB
+        } else {
+            sleepInterval = 90000; // 1.5 minutes if connected to USB
+        }
+
+        // Verificar interferencia
+        checkInterference();
+
+        // Si no se transmitieron datos, esperar un poco antes de intentar de nuevo
+        if (!datosTransmitidos) {
+            delay(5000);
+        }
     }
-    verificaYReconectaWiFi();
-    verificaIntervalosYReinicia();
 }
 
 void muestraDebugGPS() {
     while (GPSSerial.available() > 0) {
         char c = GPSSerial.read();
         if (gps.encode(c)) {
-            filteredLat = gps.location.lat();
-            filteredLng = gps.location.lng();
-
             Serial.print("GPS: Lat = ");
-            Serial.print(filteredLat, 6);
+            Serial.print(gps.location.lat(), 6);
             Serial.print(", Lng = ");
-            Serial.println(filteredLng, 6);
+            Serial.println(gps.location.lng(), 6);
         }
     }
+    int rssi = WiFi.RSSI();
+    Serial.print("RSSI: ");
+    Serial.println(rssi);
+    lastRSSI = rssi; // Guarda el valor del último RSSI leído
+
+    Serial.println("Nivel de batería: " + String(batteryVoltage));
+    Serial.println("USB_conectado: " + String(usbConnected));
 }
 
 String generaQueryString() {
@@ -108,33 +163,37 @@ String generaQueryString() {
     char timeStr[25];
     strftime(timeStr, sizeof(timeStr), "%Y-%m-%d %H:%M:%S", &timeinfo);
 
-    return String("?id=") + deviceID +
-           "&lat=" + String(filteredLat, 6) +
-           "&lng=" + String(filteredLng, 6) +
-           "&year=" + (timeinfo.tm_year + 1900) +
-           "&month=" + (timeinfo.tm_mon + 1) +
-           "&day=" + timeinfo.tm_mday +
-           "&hour=" + timeinfo.tm_hour +
-           "&minute=" + timeinfo.tm_min +
-           "&second=" + timeinfo.tm_sec +
-           "&speed=" + gps.speed.kmph() +
-           "&sat=" + gps.satellites.value() +
-           "&knots=" + gps.speed.knots() +
-           "&course=" + gps.course.deg() +
-           "&h_nav=" + h_nav +
-           "&hdop=" + gps.hdop.value();
+    String queryString = String("?id=") + deviceID +
+                         "&lat=" + String(gps.location.lat(), 6) +
+                         "&lng=" + String(gps.location.lng(), 6) +
+                         "&year=" + (timeinfo.tm_year + 1900) +
+                         "&month=" + (timeinfo.tm_mon + 1) +
+                         "&day=" + timeinfo.tm_mday +
+                         "&hour=" + timeinfo.tm_hour +
+                         "&minute=" + timeinfo.tm_min +
+                         "&second=" + timeinfo.tm_sec +
+                         "&speed=" + gps.speed.kmph() +
+                         "&sat=" + gps.satellites.value() +
+                         "&knots=" + gps.speed.knots() +
+                         "&course=" + gps.course.deg() +
+                         "&h_nav=" + h_nav +
+                         "&hdop=" + gps.hdop.value();
+    
+    return queryString;
 }
 
-void enviaDatosGPS(const String& queryString) {
+bool enviaDatosGPS(const String& queryString) {
     HTTPClient http;
     String fullURL = SERVER_URL + queryString;
+    Serial.println("Intentando conectar a: " + fullURL);
     http.begin(fullURL);
     http.setTimeout(15000);
     int httpCode = http.GET();
 
     if (httpCode > 0) {
-        if (httpCode == HTTP_CODE_OK) {
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY || httpCode == HTTP_CODE_FOUND) {
             Serial.println("Datos enviados exitosamente: " + http.getString());
+            return true; // Datos transmitidos exitosamente
         } else {
             Serial.print("Error en la solicitud HTTP: ");
             Serial.println(httpCode);
@@ -145,6 +204,7 @@ void enviaDatosGPS(const String& queryString) {
         Serial.println(http.errorToString(httpCode));
     }
     http.end();
+    return false;
 }
 
 void configuraWiFi() {
@@ -165,18 +225,37 @@ void configuraWiFi() {
 
 void setupOTA() {
     ArduinoOTA.setHostname(HOSTNAME);  // Set the hostname, defined in config.h
+    ArduinoOTA.setPassword(OTA_PASSWORD);  // Set the OTA password
 
     ArduinoOTA.onStart([]() {
-        Serial.println("Start OTA");
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH) {
+            type = "sketch";
+        } else { // U_SPIFFS
+            type = "filesystem";
+        }
+        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+        Serial.println("Start updating " + type);
     });
     ArduinoOTA.onEnd([]() {
-        Serial.println("\nEnd OTA");
+        Serial.println("\nEnd");
     });
     ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
         Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
     });
     ArduinoOTA.onError([](ota_error_t error) {
         Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) {
+            Serial.println("Auth Failed");
+        } else if (error == OTA_BEGIN_ERROR) {
+            Serial.println("Begin Failed");
+        } else if (error == OTA_CONNECT_ERROR) {
+            Serial.println("Connect Failed");
+        } else if (error == OTA_RECEIVE_ERROR) {
+            Serial.println("Receive Failed");
+        } else if (error == OTA_END_ERROR) {
+            Serial.println("End Failed");
+        }
     });
 
     ArduinoOTA.begin();
@@ -191,14 +270,6 @@ void verificaYReconectaWiFi() {
             Serial.print(".");
         }
         Serial.println("\nReconectado a WiFi.");
-    }
-}
-
-void verificaIntervalosYReinicia() {
-    unsigned long currentTime = millis();
-    if ((currentTime - lastGPSUpdateTime > GPS_UPDATE_INTERVAL) || (currentTime - lastDataSendTime > DATA_SEND_INTERVAL)) {
-        Serial.println("Reinicio necesario: intervalos máximos excedidos sin actualización del GPS o envío de datos.");
-        ESP.restart();  // Restart the microcontroller
     }
 }
 
@@ -247,11 +318,28 @@ void enviarDatosATiempo() {
     Blynk.virtualWrite(V7, gps.satellites.value());
     Blynk.virtualWrite(V8, gps.speed.knots());
     Blynk.virtualWrite(V9, deviceID);  // Asegúrate de enviar el deviceID como String
-    Blynk.virtualWrite(V10, String(filteredLat, 6));
-    Blynk.virtualWrite(V11, String(filteredLng, 6));
+    Blynk.virtualWrite(V10, String(gps.location.lat(), 6));
+    Blynk.virtualWrite(V11, String(gps.location.lng(), 6));
 }
 
 void obtenerTiempo(struct tm &timeinfo) {
     time_t local = rtc.getEpoch();
     localtime_r(&local, &timeinfo);
+}
+
+void verificarYEntrarSleep() {
+    Serial.println("Entrando en modo de suspensión...");
+    esp_sleep_enable_timer_wakeup(sleepInterval * 1000); // Configurar el temporizador de despertar
+    esp_deep_sleep_start();
+}
+
+void checkInterference() {
+    int rssi = WiFi.RSSI();
+    if (rssi < RSSI_THRESHOLD && lastRSSI >= RSSI_THRESHOLD) {
+        interferenceDetected = true;
+        Serial.println("Interferencia detectada: RSSI ha caído abruptamente");
+        // Puedes enviar una alerta a través de Blynk
+        Blynk.virtualWrite(V16, "Interferencia detectada");
+    }
+    lastRSSI = rssi; // Actualiza el último valor de RSSI leído
 }
